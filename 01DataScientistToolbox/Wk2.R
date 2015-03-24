212ec08054ac6cfbcdcb722fbc6a1a3276d4ff45
michaelpboyle <mpboyle1@rcn.com>
2015-03-23 22:10:05 -0400

    Adding simple R scripts from first class in specialization

---
+++ 01DataScientistToolbox/Wk2.R
@@ -0,0 +1,12 @@
+##### R Packages
+## a <- available.packages()
+## head(rownames(a),50)
+## tail(head(rownames(a),2000),20)
+## source("http://bioconductor.org/biocLite.R")
+## biocLite()
+## biocLite(c("GenomicFeatures", "AnnotationDbi"))  ## Error: Cannot find xml2-config
+## library(GenomicFeatures)
+## library(lattice)
+## search()
+install.packages("KernSmooth")
+library(KernSmooth)
